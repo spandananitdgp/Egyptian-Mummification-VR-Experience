63d5387ebb361408ff37b3432de45d1e4f867a6d
Sayak Biswas <sayak90@gmail.com>
2016-02-15 20:27:47 -0500

    Changelog: 1. Created Scripts Folder. 2. Added Flame SFX to all torches. 3. Added Dirt falling SFX to sandfalls. 4. Created SandAudio.cs script to sync audio looping with the visual effect. 5. Updated Readme.md to include accredition for flame and dirt SFX.

---
+++ Assets/Scripts/SandAudio.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CardboardAudioSource))]
+public class SandAudio : MonoBehaviour {
+	private ParticleSystem sandSystem;
+	private CardboardAudioSource sandFallingAudioSource;
+
+	// Use this for initialization
+	void Start () {
+		sandSystem = GetComponent<ParticleSystem> ();
+		sandFallingAudioSource = GetComponent<CardboardAudioSource> ();
+		sandFallingAudioSource.volume = 0.06f;
+		sandFallingAudioSource.pitch = 0.5f;
+		sandFallingAudioSource.rolloffMode = AudioRolloffMode.Linear;
+		sandFallingAudioSource.minDistance = 0;
+		sandFallingAudioSource.maxDistance = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float time = sandSystem.time;
+		if(Mathf.Round(time) % 4 == 0) {
+			sandFallingAudioSource.Play();
+		}
+	}
+}
