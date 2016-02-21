e634931be9315456ab6a179aa8dfbbade470ca58
Spandana <spandu.1011@gmail.com>
2016-02-21 12:58:26 -0500

    Added script for changing scenes

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+	public string sceneName;
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Player") {
+			SceneManager.LoadScene (sceneName);
+		}
+	}
+}
